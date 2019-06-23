6e9db2fdd163eb2a6764edc8a868b3f6d4b1f429
Rodri Zapico <rodri.zapico@gmail.com>
2019-06-23 01:41:02 -0300

    Added support for help, mute and default messages. Also added login logic to obtain a valid access token from the API, and reestructured the code so it is a lot tidier

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+from flask import json
 import requests
 import time
 
@@ -8,25 +10,90 @@ app = Flask(__name__)
 def ping_pong():
   return 'pong'
 
-@app.route('/tito', methods=['POST'])
-def tito_help():
-  time.sleep(1)
-  url = 'http://api.hypechat:3000/workspaces/1/messages'
+def auth_expired(res):
+  return res.status_code == 401 and res.json()['status'] == 'error' and res.json()['type'] == 'unauthorized'
 
-  headers = {
-    'X-Auth': 'Pi0BqihRDjhjtLi46gvLrrWAt9AgMvZvWWCttopgPcL11RXuDGTbFQIJRyux66A3eVDIhW6lTZnfzIxxsCY4BB93TBQKRLRxg5f3wc5Lumb4D7WauZ6WudvxQRBoKiEN'
+def login():
+  loginUrl = 'http://api.hypechat:3000/login'
+
+  loginCredentials = {
+    'email': 'tito.bot@gmail.com',
+    'password': 'titosPassword123!'
   }
 
-  message = ( 'Usage: \n'
-              '@tito help - muestra este mensaje de ayuda.\n'
-              '@tito info - muestra información del canal: integrantes, cantidad de mensajes, etc.\n'
-              '@tito mute <n> - desactiva el bot por n segundos (ejemplo: "@tito mute 30").\n'
-              '@tito me - muestra información sobre el usuario que envía el mensaje.' )
+  r = requests.post(loginUrl, json = loginCredentials)
+  return r.json()['accessToken']
+
+authToken = login()
+url = 'http://api.hypechat:3000/workspaces/1/messages'
+silencedUntil = None
+
+def help_message(received):
+  return ('Comandos: \n'
+          '@tito help - muestra este mensaje de ayuda.\n'
+          '@tito info - muestra información del canal: integrantes, cantidad de mensajes, etc.\n'
+          '@tito mute <n> - desactiva mis respuestas por n segundos. Ejemplo: "@tito mute 30" (máximo 300 segundos).\n'
+          '@tito me - muestra información sobre el usuario que envía el mensaje.')
+
+def default_message(received):
+  return ('No comprendí el comando. Envía @tito help para ver los comandos a los que puedo responder.')
+
+def mute_message(received):
+  global silencedUntil
+
+  if not received['message_tokens'][1].isdigit():
+    return default_message(received)
+
+  silencedTime = int(received['message_tokens'][1])
+  if silencedTime > 300:
+    message = ( 'Entendido, no responderé más comandos durante 300 segundos, '
+                'el máximo tiempo que puedo permanecer silenciado' )
+    silencedTime = 300
+  else:
+    message = ( 'Entendido, no responderé más comandos durante ' +
+                str(silencedTime) +
+                (' segundos' if silencedTime > 1 else ' segundo') )
+
+  silencedUntil = time.time() + silencedTime
+  return message
+
+def getAnswer(received):
+
+  received['message_tokens'] = received['message'].split()
+  return {
+    'help': help_message,
+    'mute': mute_message,
+  }.get(received['message_tokens'][0].lower(), default_message)(received)
+
+def send_response(message):
+  headers = {
+    'X-Auth': authToken
+  }
+
   payload = {
     'recipientId': None,
     'groupId': 1,
     'message': message
   }
 
-  r = requests.post(url, headers = headers, json = payload)
+  return requests.post(url, headers = headers, json = payload)
+
+@app.route('/tito', methods=['POST'])
+def tito_help():
+  global authToken
+  print("received: " + json.dumps(request.json), flush = True)
+
+  # If bot is silenced, return.
+  if silencedUntil and silencedUntil > time.time():
+    return "200"
+
+  time.sleep(1)
+
+  anwser = getAnswer(request.json)
+  r = send_response(anwser)
+
+  if auth_expired(r):
+    authToken = login()
+    send_response(anwser)
+
   return "200"
